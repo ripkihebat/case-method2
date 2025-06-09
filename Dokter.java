5ff5ae79c0e1a6d26996b159ea7a3b912bba41e2
nesslty <pineapplekuu@gmail.com>
2025-06-09 11:16:30 +0700

    dokter

---
+++ Dokter.java
@@ -0,0 +1,9 @@
+public class Dokter {
+    String idDokter;
+    String nama;
+
+    public Dokter(String idDokter, String nama) {
+        this.idDokter = idDokter;
+        this.nama = nama;
+    }
+}
