89ce93bac0a7c71a9a8f34baff3ba9c2b4f066dc
ripkihebat <ripkeyajah@gmail.com>
2025-06-09 10:47:59 +0700

    Delete tes directory

--- tes/Dokter08.java
+++
@@ -1,9 +0,0 @@
-public class Dokter08 {
-    String idDokter;
-    String nama;
-
-    public Dokter08 (String idDokter, String nama) {
-        this.idDokter = idDokter;
-        this.nama = nama;
-    }
-}
--- tes/NodePasien08.java
+++
@@ -1,9 +0,0 @@
-public class NodePasien08 {
-    Pasien08 data;
-    NodePasien08 next;
-    
-    public NodePasien08 (Pasien08 data, NodePasien08 next){
-        this.data = data;
-        this.next = next;
-    }
-}
